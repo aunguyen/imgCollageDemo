5ab29a179205e61f459094d46dc36b2c4b1aca7c
aunguyen <aunguyen1011@gmail.com>
2016-01-12 00:10:52 +0700

    add project

--- ImgCollage/ViewController.m
+++ ImgCollage/ViewController.m
@@ -8,7 +8,24 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIScrollViewDelegate>{
+    UITapGestureRecognizer* tap;
+    UIPanGestureRecognizer* pan;
+    UIPinchGestureRecognizer* pinch;
+    UILongPressGestureRecognizer* longPress;
+    UIImagePickerController* ipc;
+    UIView* tempImgView;
+    UIImageView* tempIV;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *img1;
+@property (weak, nonatomic) IBOutlet UIImageView *img2;
+@property (weak, nonatomic) IBOutlet UIImageView *img3;
+@property (weak, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UIView *view2;
+@property (weak, nonatomic) IBOutlet UIView *view3;
+@property (weak, nonatomic) IBOutlet UIScrollView *scroll1;
+@property (weak, nonatomic) IBOutlet UIScrollView *scroll2;
+@property (weak, nonatomic) IBOutlet UIScrollView *scroll3;
 
 @end
 
@@ -17,6 +34,120 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initZoomForScrollView:_scroll1];
+    [self initZoomForScrollView:_scroll2];
+    [self initZoomForScrollView:_scroll3];
+    [self addGestureForView:_scroll1];
+    [self addGestureForView:_scroll2];
+    [self addGestureForView:_scroll3];
+    [self setupLayout];
+}
+
+- (void)addGestureForView:(UIView*)view{
+    pinch = [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(scaleImg:)];
+    tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(selectImg:)];
+    longPress = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(getImg:)];
+    [longPress setMinimumPressDuration:1];
+    
+    [view viewWithTag:100].userInteractionEnabled = YES;
+    view.userInteractionEnabled = YES;
+    [view addGestureRecognizer:pinch];
+    [view addGestureRecognizer:tap];
+    [[view viewWithTag:100] addGestureRecognizer:longPress];
+}
+
+- (void)initZoomForScrollView:(UIScrollView*)sv{
+    sv.delegate = self;
+    sv.showsHorizontalScrollIndicator = NO;
+    sv.showsVerticalScrollIndicator = NO;
+    sv.minimumZoomScale = 1.f;
+    sv.maximumZoomScale = 4.f;
+}
+
+- (void)setupLayout{
+    _scroll1.layer.borderColor = [UIColor greenColor].CGColor;
+    _scroll2.layer.borderColor = [UIColor greenColor].CGColor;
+    _scroll3.layer.borderColor = [UIColor greenColor].CGColor;
+    
+    _scroll1.layer.borderWidth = 1.5;
+    _scroll2.layer.borderWidth = 1.5;
+    _scroll3.layer.borderWidth = 1.5;
+    
+    tempIV = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 200, 120)];
+    tempIV.alpha = 0;
+    [self.view addSubview:tempIV];
+    
+}
+
+- (void)selectImg:(UITapGestureRecognizer*)tapGesture{
+    tempImgView = tapGesture.view;
+    [self showImgPickerForView:tempImgView];
+}
+
+- (void)getImg:(UILongPressGestureRecognizer*)longGesture{
+    CGPoint location = [longGesture locationInView:self.view];
+    UIImage* img = [(UIImageView*)longGesture.view image];
+    
+    if (longGesture.state == UIGestureRecognizerStateBegan) {
+        tempIV.image = img;
+        tempIV.alpha = 0.7;
+        tempIV.center = location;
+    }else if (longGesture.state == UIGestureRecognizerStateChanged){
+        tempIV.center = location;
+    }else if (longGesture.state == UIGestureRecognizerStateEnded){
+        
+        if (CGRectContainsPoint(_view1.frame, location)) {
+            _img1.image = tempIV.image;
+        }
+        
+        if (CGRectContainsPoint(_view2.frame, location)) {
+            _img2.image = tempIV.image;
+        }
+
+        if (CGRectContainsPoint(_view3.frame, location)) {
+            _img3.image = tempIV.image;
+        }
+        
+        tempIV.image = nil;
+        tempIV.alpha = 0;
+    }
+    
+    
+}
+
+- (void)scaleImg:(UIPinchGestureRecognizer*)pinchGesture{
+    [((UIScrollView*)pinchGesture.view) setZoomScale:pinchGesture.scale animated:YES];
+}
+
+
+- (void)showImgPickerForView:(UIView*)view{
+    ipc = [[UIImagePickerController alloc] init];
+    ipc.delegate = self;
+    ipc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    [self presentViewController:ipc animated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    ((UIImageView*)[tempImgView viewWithTag:100]).image = [info objectForKey:UIImagePickerControllerOriginalImage];
+}
+
+
+- (UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return [scrollView viewWithTag:100];
+}
+
+- (IBAction)reset:(id)sender {
+    
+    UIImage* image = [UIImage imageNamed:@"addImg"];
+    _img1.image = image;
+    _img2.image = image;
+    _img3.image = image;
+    
+    [_scroll1 setZoomScale:1];
+    [_scroll2 setZoomScale:1];
+    [_scroll3 setZoomScale:1];
 }
 
 - (void)didReceiveMemoryWarning {
